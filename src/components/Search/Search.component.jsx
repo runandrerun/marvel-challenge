c5abf6e5976552edce171b34e24927082fc297d4
runandrerun <the.asantiagojr@gmail.com>
2020-11-21 16:10:07 -0500

    Setting up containers and search component.

--- src/components/Search/Search.component.jsx
+++ src/components/Search/Search.component.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState, useContext, createContext } from 'react';
 import {Container, Input} from './Search.styles';
 
+const SearchContext = createContext();
+
 export default function Search({ children, ...restProps }) {
+  const [giphyList, setGiphyList] = useState("");
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+  };
   return(
     <Container {...restProps}>{children}</Container>
   );
 };
 
 Search.Input = function SearchInput({ ...restProps }) {
+  const { giphyList, setGiphyList } = useContext(SearchContext);
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+  };
   return (
-    <Input {...restProps} />
+    <Input
+      {...restProps}
+      onChange={(value) => setGiphyList(value)}
+    />
   );
 };
