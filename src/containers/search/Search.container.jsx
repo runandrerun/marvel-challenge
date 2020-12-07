1da5fd6df84d2100bd7d999b426e8d2b8eae2044
runandrerun <the.asantiagojr@gmail.com>
2020-12-06 21:54:53 -0500

    Adjusted form. Created formWrap. Changed styling.

--- src/containers/search/Search.container.jsx
+++ src/containers/search/Search.container.jsx
@@ -4,8 +4,10 @@ import {Search} from '../../components';
 export default function SearchContainer() {
   return (
     <Search>
-      <Search.Input placeholder={"Search for a gif"}/>
-      <Search.Button>Search</Search.Button>
+      <Search.FormWrap>
+        <Search.Input placeholder={"Search for a gif"}/>
+        <Search.Button>Search</Search.Button>
+      </Search.FormWrap>
     </Search>
   );
 };
