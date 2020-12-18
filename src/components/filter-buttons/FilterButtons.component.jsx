bd5f32fe5b5527fd71ab023dff2495320badf61e
runandrerun <the.asantiagojr@gmail.com>
2020-12-17 22:14:02 -0500

    Setup of filter buttons section.

---
+++ src/components/filter-buttons/FilterButtons.component.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Container} from '.FilterButtons.styles';
+
+export default function FilterButtons({ children, ...restProps }) {
+  return (
+    <Container {...restProps}>{children}</Container>
+  );
+};
